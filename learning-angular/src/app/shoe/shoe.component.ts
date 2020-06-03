95ca428777d8eb6d04274d4af3db756c0b82fc33
Geovanny <giorc93@hotmail.com>
2020-06-03 16:27:04 -0500

    Third Commit: Structural directives

--- learning-angular/src/app/shoe/shoe.component.ts
+++ learning-angular/src/app/shoe/shoe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 import { Shoe } from '../models/shoe';
+import { newArray } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-shoe',
@@ -10,19 +11,24 @@ import { Shoe } from '../models/shoe';
 export class ShoeComponent implements OnInit, DoCheck, OnDestroy {
   public title: string = 'Shoes List';
   public shoes: Array<Shoe>;
+  public brands: string[];
+  public color: string;
 
   constructor() {
+    this.color = 'brown';
+    this.brands = new Array();
     this.shoes = [
       new Shoe('Reebok Classic', 'Reebook', 'White', 40.99, true),
-      new Shoe('Adidas Samba', 'Adidas', 'Black', 40.99, true),
+      new Shoe('Adidas Samba', 'Adidas', 'Black', 24.99, true),
       new Shoe('Adidas Pace', 'Adidas', 'White', 50.99, true),
+      new Shoe('Adidas Runner', 'Adidas', 'White', 29.99, true),
     ];
   }
 
   //ngOnInit: Executes once the component has been loaded
   ngOnInit(): void {
     console.log('OnInit executed');
-    console.log(this.shoes);
+    this.listBrands();
   }
   //ngDoCheck: Executes everytime any change on the component or the app is made
   ngDoCheck(): void {
@@ -35,5 +41,15 @@ export class ShoeComponent implements OnInit, DoCheck, OnDestroy {
 
   setTitle() {
     this.title = 'Shoe List & Description';
+  }
+
+  listBrands() {
+    this.shoes.forEach((shoe, index) => {
+      if (this.brands.indexOf(shoe.brand) < 0) {
+        //If the element already exits, skips the push method to avoid duplicates
+        this.brands.push(shoe.brand);
+      }
+    });
+    console.log(this.brands);
   }
 }
