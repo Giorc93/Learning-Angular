805d5617ad14e5070041b5310847fac629e59d7f
Geovanny <giorc93@hotmail.com>
2020-06-03 23:30:54 -0500

    8th Commit: If/Else/Then

--- learning-angular/src/app/home/home.component.ts
+++ learning-angular/src/app/home/home.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  public identified: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.identified = false;
   }
 
+  ngOnInit(): void {}
+
+  logIn() {
+    this.identified = true;
+  }
+
+  logOut() {
+    this.identified = false;
+  }
 }
